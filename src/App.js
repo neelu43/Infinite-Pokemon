21abaa40d97a24987b671a78b4a21785f89ecb38
neelu43 <86164992+neelu43@users.noreply.github.com>
2023-07-03 10:57:29 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-import './App.css';
+import "./App.css";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
+  
+  const [items, setItems] = useState([]);
+  const ref = useRef(null);
+  const randomId = () => {
+    return Math.floor(Math.random() * 500 + 1);
+  };
+
+  const getPokemon = async (id) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+    const data = await response.json();
+
+    setItems(
+      items.concat({image:data.sprites.other.dream_world.front_default, name:data.name})
+    );
+  };
+
+  function useOnScreen(ref) {
+
+    const [isIntersecting, setIntersecting] = useState(false)
+  
+    const observer = new IntersectionObserver(
+      ([entry]) => setIntersecting(entry.isIntersecting)
+    )
+  
+    useEffect(() => {
+      observer.observe(ref.current)
+      return () => { observer.disconnect() }
+    }, [])
+  
+    return isIntersecting
+  }
+
+
+    if(useOnScreen(ref)){
+      getPokemon(randomId())
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <header className="heading">Infinite Pokemons</header>
+      <div className="main">
+        {items.map((p) => (
+          <div className="card">
+            <img src={p.image} className="cardImage"/>
+            {p.name}
+          </div>)
+        )}
+      </div>
+      <div ref={ref}></div>
+      <br />
+      <br />
+      <br />
+
     </div>
   );
 }
